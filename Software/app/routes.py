78a37ffa9cdf677cc71e2db7a3ce96e26f44dfcf
Connor <connor1031levinson@gmail.com>
2023-11-02 18:54:21 -0400

    Move over flask tutorial code

---
+++ Software/app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # POST method validates
+        flash('Login requested for user {}, '
+              'remember_me={}'.format(form.username.data,
+                                      form.remember_me.data))
+    # flash to display messages for prototype, driver
+        return redirect(url_for('index'))  # navigation when complete
+    return render_template('login.html',
+                           title ='Sign In',
+                           form = form)
