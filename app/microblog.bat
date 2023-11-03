f341543bc6490cb9122515a6c1ced289293603df
Connor <connor1031levinson@gmail.com>
2023-11-02 21:08:40 -0400

    version 3 with coach and player view

--- app/microblog.bat
+++
@@ -1,2 +0,0 @@
-set FLASK_APP=microblog.py
-flask run
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,4 +17,15 @@ def index():
     ]
     return render_template('index.html', title ='Home', user=user, posts=posts)
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In',
+                           form=form)
+@app.route('/coach')
+def coach():
+    return render_template('coach.html', title='Coach')
 
+@app.route('/player')
+def player():
+    return render_template('player.html', title='Player')
