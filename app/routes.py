3d5e863c3c3efa14946a7cfd1100ac2b2e06fdae
Connor <connor1031levinson@gmail.com>
2023-11-03 09:11:40 -0400

    Adding more versatile coach and player views

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,33 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, PlayerForm, CoachForm
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title ='Home')
+    return render_template('index.html', title='Home')
+
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title='Sign In')
-@app.route('/coach')
-def coach():
-    return render_template('coach.html', title='Coach')
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/player')
+
+@app.route('/coach', methods=['GET', 'POST'])
+def coach():
+    form = CoachForm()
+    if form.validate_on_submit():
+        # Process the form data (e.g., save it to a database)
+        return redirect(url_for('coach'))  # Replace 'success' with your success page
+    return render_template('coach.html', form=form)
+
+
+@app.route('/player', methods=['GET', 'POST'])
 def player():
-    return render_template('player.html', title='Player')
+    form = PlayerForm()
+    if form.validate_on_submit():
+        # Process the form data (e.g., save it to a database)
+        return redirect(url_for('player'))  # Replace 'success' with your success page
+    return render_template('player.html', form=form)
